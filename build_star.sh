4dff17dd48b594d921140e8be2858e846fba06cc
ikun-1145141 <334495606@qq.com>
2026-09-23 10:06:57 +0000

    zram: add OPlus ZSTDn as optional test module

--- build_star.sh
+++ build_star.sh
@@ -61,6 +61,7 @@ scripts/config --file "$OUT_DIR/.config" \
   --enable CRYPTO_LZ4K \
   --enable CRYPTO_LZ4KD \
   --enable CRYPTO_LZ4P \
+  --module CRYPTO_ZSTDN \
   --enable ZRAM_DEF_COMP_LZ4 \
   --enable F2FS_FS_COMPRESSION \
   --enable F2FS_FS_LZO \
--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -40,6 +40,9 @@ static const char * const backends[] = {
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
 	"zstd",
 #endif
+#if IS_ENABLED(CONFIG_CRYPTO_ZSTDN)
+	"zstdn",
+#endif
 	NULL
 };
 
