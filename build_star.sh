6a8474db42a950f63c1f5028a26938b58f430ff8
ikun-1145141 <334495606@qq.com>
2026-09-23 09:51:38 +0000

    zram: add optional LZ4KD compressor

--- build_star.sh
+++ build_star.sh
@@ -59,6 +59,7 @@ scripts/config --file "$OUT_DIR/.config" \
   --enable CRYPTO_LZO \
   --enable CRYPTO_LZ4 \
   --enable CRYPTO_LZ4K \
+  --enable CRYPTO_LZ4KD \
   --enable ZRAM_DEF_COMP_LZ4 \
   --enable F2FS_FS_COMPRESSION \
   --enable F2FS_FS_LZO \
